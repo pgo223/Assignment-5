a2386d8aaab0f526751339478ddd95b6cf55f23f
Peter Owens <pgo223@lehigh.edu>
2019-11-15 00:29:09 -0500

    Livs suggestions

--- Assignment 5 R Script.R
+++ Assignment 5 R Script.R
@@ -1,6 +1,8 @@
 if (!require("data.table")) install.packages("data.table")
 library("data.table")
-if (!require("readr")) install.packages("readr")
-library("readr")
-data.table::fread("DEXUSAL.csv")
-DF <- fread("DEXUSAL.csv", header="auto", data.table=FALSE)
+header <- read.table("DEXUSAL.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("DEXUSAL.csv", skip=1, sep=",",
+            header=FALSE, data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
